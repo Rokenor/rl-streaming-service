abce2935a6ef6161cd3854c53a956af259a93221
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-01-21 22:45:53 +0200

    client oauth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -7,23 +7,54 @@ class GoogleAuth extends React.Component {
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId: '218132036971-ebu44gm2d7dfkn4pflgoqjbofo52jvri.apps.googleusercontent.com',
-        scope: 'email'
-      }).then(() => {
-        this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-      }) 
+      window.gapi.client
+        .init({
+          clientId: '218132036971-ebu44gm2d7dfkn4pflgoqjbofo52jvri.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        }) 
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I dont know if we are signed in</div>
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>
+      return (
+        <button 
+          className="ui red google button"
+          onClick={this.onSignOut}
+        >
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>
+      return (
+        <button 
+          className="ui red google button"
+          onClick={this.onSignIn}
+        >
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 
