ed174b5d1740dec4bfc4c524c14dc3ece54d3a8a
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-01-26 20:11:24 +0200

    client programmatic navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+
+import history from '../history';
 
 import Header from './Header';
 
@@ -12,14 +14,14 @@ import StreamShow from './streams/StreamShow';
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" component={StreamCreate} />
         <Route path="/streams/edit" component={StreamEdit} />
         <Route path="/streams/delete" component={StreamDelete} />
         <Route path="/streams/show" component={StreamShow} />
-      </BrowserRouter>
+      </Router>
     </div>
   )
 }
